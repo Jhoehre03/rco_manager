6325e6ae61d34502b221e295dcd921c4dea936c8
Jhoehre03 <jhonyr.work1@gmail.com>
2026-03-22 11:38:57 -0300

    escolas: listagem de escolas e turmas por escola funcionando

--- teste_auth.py
+++ teste_auth.py
@@ -1,4 +1,18 @@
 from rco.auth import conectar_chrome
+from rco.escolas import get_escolas, get_escolas_turmas
 
 browser = conectar_chrome()
-print("Conectado! Título da página:", browser.title)
+print("Conectado:", browser.title)
+
+print("\nEscolas:")
+for e in get_escolas(browser):
+    print(" -", e)
+
+print("\nTurmas por escola:")
+turmas = get_escolas_turmas(browser)
+escola_atual = ""
+for t in turmas:
+    if t["escola"] != escola_atual:
+        escola_atual = t["escola"]
+        print(f"\n  {escola_atual}")
+    print(f"    - {t['turma']} | {t['disciplina']}")
